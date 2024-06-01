c2b04daac64268ba823123d940f5bb445201be8f
“KellyReisLee” <kristinaalvares28@gmail.com>
2024-06-01 22:02:48 +0100

    adding changes

--- backend/modules/Cart.js
+++ backend/modules/Cart.js
@@ -2,22 +2,16 @@ const mongoose = require('mongoose')
 
 const CartSchema = new mongoose.Schema({
   userId: { type: String, required: true },
-  products: [
-    {
-      productId: {
-        type: String
-      },
-      quantity: {
-        type: Number,
-        default: 1
-      }
-    }
-  ]
-
-
-
+  productId: {
+    type: String
+  },
+  quantity: {
+    type: Number,
+    default: 1
+  }
 }, { timestamps: true })
 
 const cartModel = mongoose.model('Cart', CartSchema)
 
 module.exports = cartModel
+
